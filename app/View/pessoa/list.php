daa5106cb79a555a46450e3074b1c707c23a5ae4
enzovarellap <enzovpastore@gmail.com>
2025-05-22 14:32:12 -0300

    chore: create table to List all Pessoas on database

--- app/View/pessoa/list.php
+++ app/View/pessoa/list.php
@@ -1,6 +1,19 @@
 
-<?php foreach ($pessoas as $pessoa): ?>
-    <div>
-        <?= htmlspecialchars($pessoa->getNome()) ?> - <?= htmlspecialchars($pessoa->getCpf()) ?>
-    </div>
-<?php endforeach; ?>
+<table border="2">
+    <thead>
+    <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>CPF</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php foreach ($pessoas as $pessoa): ?>
+        <tr>
+            <td><?= htmlspecialchars($pessoa->getId()) ?></td>
+            <td><?= htmlspecialchars($pessoa->getNome()) ?></td>
+            <td><?= htmlspecialchars($pessoa->getFormatedCpf()) ?></td>
+        </tr>
+    <?php endforeach; ?>
+    </tbody>
+</table>
